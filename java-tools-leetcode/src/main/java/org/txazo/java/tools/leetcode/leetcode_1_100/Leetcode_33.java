a7ee6205dceebd84b09935e13561439b51cf9491
txazo <784990655@qq.com>
2021-07-25 22:46:58 +0800

    auto commit

--- java-tools-leetcode/src/main/java/org/txazo/java/tools/leetcode/leetcode_1_100/Leetcode_33.java
+++ java-tools-leetcode/src/main/java/org/txazo/java/tools/leetcode/leetcode_1_100/Leetcode_33.java
@@ -1,8 +1,15 @@
 package org.txazo.java.tools.leetcode.leetcode_1_100;
 
 /**
- * @author tuxiaozhou
- * @date 2021/7/24
+ * 33、搜索旋转排序数组
+ * <p>
+ * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+ * <p>
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
  */
 public class Leetcode_33 {
 
